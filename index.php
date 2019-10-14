a8809880e23f1074a44255702d5144401be627c3
Abhishek Mundada <42415137+Vault09@users.noreply.github.com>
2019-10-14 19:40:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,66 @@
 <!DOCTYPE html>
-<html>
-<title>Abhishek Mundada</title>
-<h1>Abhishek Mundada Request/Response</h1>
-
-<?php
-echo "The SHA256 hash of \"Abhishek Mundada\" is ";
-print hash('sha256', 'Abhishek Mundada');
-?>
+<head><title>Abhishek Mundada MD5 Cracker</title></head>
 <body>
-<br><br>
-ASCII ART:
+<h1>MD5 cracker</h1>
+<p>This application takes an MD5 hash
+of a four-character lower case string and 
+attempts to hash all four-character combinations
+to determine the original four characters.</p>
 <pre>
-     ***** 
-    **   ** 
-    *******
-    **   **
-    **   **
+Debug Output:
+<?php
+$goodtext = "Not found";
+if ( isset($_GET['md5']) ) {
+    $time_pre = microtime(true);
+    $md5 = $_GET['md5'];
+
+    $txt = "abcdefghijklmnopqrstuvwxyz";
+    $show = 20;
+
+    for($i=0; $i<strlen($txt); $i++ ) {
+        $ch1 = $txt[$i];
+
+        for($j=0; $j<strlen($txt); $j++ ) {
+            $ch2 = $txt[$j];
+            
+            for($k=0; $k<strlen($txt); $k++ ) {
+                $ch3 = $txt[$k];
+
+                for($l=0; $l<strlen($txt); $l++ ) {
+                    $ch4 = $txt[$l];
+            $try = $ch1.$ch2.$ch3.$ch4;
+
+            $check = hash('md5', $try);
+            if ( $check == $md5 ) {
+                $goodtext = $try;
+                break;   
+            }
+
+            if ( $show > 0 ) {
+                print "$check $try\n";
+                $show = $show - 1;
+            }
+        }
+    }
+}
+}
+    $time_post = microtime(true);
+    print "Elapsed time: ";
+    print $time_post-$time_pre;
+    print "\n";
+}
+?>
 </pre>
-<br>
-<a href ="http://localhost/check.php">Click here to check the error setting</a>
-<br>
-<a href ="http://localhost/fail.php">Click here to cause a traceback</a>
+<p>Original Text: <?= htmlentities($goodtext); ?></p>
+<form>
+<input type="text" name="md5" size="60" />
+<input type="submit" value="Crack MD5"/>
+</form>
+<ul>
+<li><a href="index.php">Reset</a></li>
+<li><a href="md5.php">MD5 Encoder</a></li>
+<li><a href="makecode.php">MD5 Code Maker</a></li>
+<li><a href="https://github.com/Vault09/PHP_wa4e/tree/Hashing" target="_blank">Source code for this application</a></li>
+</ul>
 </body>
 </html>
