f13c36714f8f404356c45a9ec5ab467b31ac2197
Abhishek Mundada <42415137+Vault09@users.noreply.github.com>
2019-10-15 11:02:51 +0530

    Updated numeric encoder

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 <head><title>Abhishek Mundada MD5 Cracker</title></head>
 <body>
 <h1>MD5 cracker</h1>
-<p>This application takes an MD5 hash
-of a four-character lower case string and 
-attempts to hash all four-character combinations
-to determine the original four characters.</p>
+<p>
+This application takes an MD5 hash of a four digit pin and check all 10,000 possible four digit PINs to determine the PIN.
+</p>
 <pre>
 Debug Output:
 <?php
-$goodtext = "Not found";
+$total_checks = 0;
+$goodpin = "Not found";
 if ( isset($_GET['md5']) ) {
     $time_pre = microtime(true);
     $md5 = $_GET['md5'];
 
-    $txt = "abcdefghijklmnopqrstuvwxyz";
+    $txt = "0123456789";
     $show = 20;
 
     for($i=0; $i<strlen($txt); $i++ ) {
@@ -32,7 +32,8 @@ if ( isset($_GET['md5']) ) {
 
             $check = hash('md5', $try);
             if ( $check == $md5 ) {
-                $goodtext = $try;
+                $goodpin = $try;
+                $total_checks = $total_checks + 1;
                 break;   
             }
 
@@ -51,16 +52,17 @@ if ( isset($_GET['md5']) ) {
 }
 ?>
 </pre>
-<p>Original Text: <?= htmlentities($goodtext); ?></p>
+<p>PIN: <?= htmlentities($goodpin); ?></p>
 <form>
 <input type="text" name="md5" size="60" />
 <input type="submit" value="Crack MD5"/>
 </form>
 <ul>
 <li><a href="index.php">Reset</a></li>
+<li><a href="makepin.php">Make an MD5 PIN</a></li>
 <li><a href="md5.php">MD5 Encoder</a></li>
-<li><a href="makecode.php">MD5 Code Maker</a></li>
-<li><a href="https://github.com/Vault09/PHP_wa4e/tree/Hashing" target="_blank">Source code for this application</a></li>
+<li><a href="https://www.wa4e.com/assn/crack/" target="_blank">Specification for this assignment</a></li>
+<li><a href="https://github.com/Vault09/PHP_wa4e/tree/Hashing" target="_blank">Source code similar to this application</a></li>
 </ul>
 </body>
 </html>
